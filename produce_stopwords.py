9037d77a5b3b0bea63ec292efb0e1951c994e441
Stanisław Giziński <s.gizinski84@gmail.com>
2018-10-25 02:28:33 +0200

    Script for producing SW list, and list. I will remove SW list from repo in future.

---
+++ produce_stopwords.py
@@ -0,0 +1,13 @@
+import requests
+
+r=requests.get("https://raw.githubusercontent.com/bieli/stopwords/master/polish.stopwords.txt")
+bieli_sw=set(r.content.decode().split('\n')[:-1])
+
+r=requests.get("https://raw.githubusercontent.com/stopwords-iso/stopwords-pl/master/stopwords-pl.txt")
+iso_sw=set(r.content.decode().split('\n')[:-1])
+
+SW_list=iso_sw|bieli_sw
+
+with open("data/stop_words.txt", "w") as f:
+    for word in SW_list:
+        f.write("{}\n".format(word))
