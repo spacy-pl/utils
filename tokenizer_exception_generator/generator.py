57671bfa165d85cd746ee8d56625b6fc1e2729af
quark <piotr.ksiazek96@gmail.com>
2018-11-19 14:21:24 +0100

    Resolve issues after code review

--- tokenizer_exception_generator/generator.py
+++ tokenizer_exception_generator/generator.py
@@ -9,7 +9,6 @@ class Rule:
     """
     Same structure as Segment's
     """
-
     def __init__(self, breaking: bool, before_pattern_str: str = None, after_pattern_str: str = None):
         self.breaking = breaking
         self.before_pattern_str = before_pattern_str
@@ -29,10 +28,10 @@ class MyHTMLParser(HTMLParser):
 
 
 def get_rules_from_xml(filename=os.path.expanduser('./data/tokenizer_exception_generator/segment.srx')):
-    tree = ET.parse(filename)  # parse XML file
-    root = tree.getroot()  # access file's root
-
-    polish_rules = root[1][0][1]  # this is not a nice solution, but a working one
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    # this is not a nice solution, but a working one
+    polish_rules = root[1][0][1]
     # Other languagerule nodes that may interest us are GeneralImportant, ByLineBreak, ByTwoLineBreaks
 
     rules = []
@@ -42,7 +41,6 @@ def get_rules_from_xml(filename=os.path.expanduser('./data/tokenizer_exception_g
         # |- tag: beforebreak, tag.text: <regex>
         # |- tag: afterbreak, tag.text: <regex>
         #
-
         breaking = True if rule.attrib['break'] == 'yes' else False
         before_pattern_str = rule[0].text
         after_pattern_str = rule[1].text
@@ -53,22 +51,21 @@ def get_rules_from_xml(filename=os.path.expanduser('./data/tokenizer_exception_g
 
 def parse_html(filename):
     parser = MyHTMLParser()
-
-    exc_ = []
-
+    exceptions = []
     with open(filename, 'r', encoding="utf8") as f:
         parser.feed(f.read())
 
         for e in parser.exceptions:
             if e != '\n' and e.endswith('.'):
-                exc_ += [e]
+                exceptions += [e]
 
-    return exc_
+    return exceptions
 
-
-rules = get_rules_from_xml()
 
 exception_rules = []
+final_exceptions = []
+
+rules = get_rules_from_xml()
 
 for rule in rules:
     if not rule.breaking and rule.after_pattern_str is None or rule.after_pattern_str == "":
@@ -77,10 +74,7 @@ for rule in rules:
 pl_lowercase = u'aąbcćdeęfghijklłmnńoóprsśtuwyzźż'
 pl_uppercase = pl_lowercase.upper()
 
-final_exceptions = []
-
 for rule in exception_rules:
-
     # replace unicode properties with standard regex
     pattern = rule.before_pattern_str\
         .replace("\p{Lu}", "[" + pl_uppercase + "]")\
@@ -92,33 +86,26 @@ for rule in exception_rules:
     try:
         a = list(sre_yield.AllStrings(pattern))
         final_exceptions += a
-
-    except:
-        print(f"Error in {rule}")
-
-excluded_characters = ['\\', '\t', '\x0b', '\b', '\r', '\x0c', '\n', ')', '(', ',']
+    except Exception as e:
+        print(f"Error {e} in {rule}")
 
 # filter out exceptions that contain characters such as \r, remove trailing spaces
 # for example, for each X. X. type rule there was a X.\rX. rule in the XML file
-final_exceptions = [a.rstrip("\n\r ").lstrip(" ") for a in final_exceptions if not any(exc in a for exc in excluded_characters)]
-
+excluded_characters = ['\\', '\t', '\x0b', '\b', '\r', '\x0c', '\n', ')', '(', ',']
+final_exceptions = [a.rstrip("\n\r ").lstrip(" ")
+                    for a in final_exceptions
+                    if not any(exc in a for exc in excluded_characters)]
 # filter out exceptions that don't contain any letters
-final_exceptions = [fe for fe in final_exceptions if re.search(f'.*[{pl_lowercase}{pl_uppercase}].*', fe)]
-
-# avoid duplicates
+final_exceptions = [fe for fe in final_exceptions
+                    if re.search(f'.*[{pl_lowercase}{pl_uppercase}].*', fe)]
 final_exceptions = set(final_exceptions)
-
 # merge with exceptions from wikipedia
-for fn in [os.path.expanduser(f'./data/tokenizer_exception_generator/skroty{i}.html') for i in range(1,4)]:
+for fn in [os.path.expanduser(f'./data/tokenizer_exception_generator/skroty{i}.html') for i in range(1, 4)]:
     tmp = parse_html(fn)
     tmp = set(tmp)
     final_exceptions |= tmp
-   
 final_exceptions |= set(['gosp.-polit.', 'rub.', 'k.o.', 'm.st.', 'pp.'])
-
-# convert back to a sorted list
 final_exceptions = sorted(list(final_exceptions))
-
 with open(os.path.expanduser('./data/tokenizer_exception_generator/exception_list.py'), 'w+', encoding='utf-8') as f:
     f.write('polish_exceptions = ')
     f.write(str(final_exceptions))
