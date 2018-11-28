c622afa4bc5a145e848198fd044c544e714bd49f
Mateusz Olko <m.olko@bothouse.co>
2018-11-28 18:12:50 +0100

    Wrapped extact_rules with argparse

--- lemma_rules_extraction/extract_rules.py
+++ lemma_rules_extraction/extract_rules.py
@@ -1,8 +1,13 @@
-import re
+import argparse
 import json
 from collections import defaultdict, namedtuple
+
+import settings
+
 # aktualne dane pochodzą z sjp-ispell-pl-20181128-src
-Rawrule = namedtuple("Rawrule", ["suf", "lemma_end", "word_end", "comment"])
+
+SKIP_FIRST_N = 34
+
 
 def extract_comment(line):
     if line[0] == '#':
@@ -15,47 +20,71 @@ def extract_comment(line):
             comment = splitted[1]
         else:
             comment = ''
-    return line, comment
+    return line.strip(), comment.strip()
 
-with open("../data/polish.aff", "rb") as f:
-        lines = f.readlines()
 
-lines = [l.decode('iso-8859-2').strip() for l in lines]
-lines = lines[34:] # pominięcie pierwszych linii
-lines = [l.replace('\t', '') for l in lines]
-lines = [l for l in lines if l.replace(' ', '') != '']
-lines = [(l.replace(' ', ''), c.strip()) for l, c in [extract_comment(line) for line in lines]]
-lines = [l for l in lines if l[0] != '' or l[1] != '']
+def main(args):
 
-rule_groups = defaultdict(list)
-comments = defaultdict(list)
-flag = ''
-print(len(lines))
-for i, l in enumerate(lines):
-    line = l[0]
-    if line.startswith('flag*'):
-        flag = line[5]
-    elif line == '' and l[1] != '':
-        comments[flag].append(l[1])
-    elif line != '':
-        suf, ends = (line).split('>')
-        if ',' in ends:
-            lemma_end, word_end = (" " + ends + " ").split(',')
-            lemma_end = lemma_end.strip()[1:]
-            word_end = word_end.strip()
-            word_suf = suf[:-len(lemma_end)] + word_end
+    with open(args.ispell_rules, "rb") as f:
+            lines = f.readlines()
+
+    lines = [l.decode('iso-8859-2').strip() for l in lines]
+    lines = lines[SKIP_FIRST_N:]  # pominięcie pierwszych linii
+    lines = [l.replace('\t', '') for l in lines]
+    lines = [l for l in lines if l.replace(' ', '') != '']
+    lines = [(l.replace(' ', ''), c) for l, c in [extract_comment(line) for line in lines]]
+    lines = [l for l in lines if l[0] != '' or l[1] != '']
+
+    rule_groups = defaultdict(list)
+    comments = defaultdict(list)
+    flag = ''
+    lines.append(("", ""))
+    for l in lines:
+        line = l[0]
+        if line.startswith('flag*'):
+            flag = line[5]
+        elif line == '' and l[1] != '':
+            comments[flag].append(l[1])
+        elif line != '':
+            suf, ends = line.split('>')
+            if ',' in ends:
+                lemma_end, word_end = ends.split(',')
+                lemma_end = lemma_end.strip()[1:]
+                word_end = word_end.strip()
+                word_suf = suf[:-len(lemma_end)] + word_end
+            else:
+                lemma_end = ''
+                word_end = ends
+                word_suf = suf + word_end
+            rule_groups[flag].append({'word_suf': word_suf, "lemma_suf": suf})
         else:
-            lemma_end = ''
-            word_end = ends
-            word_suf = suf + word_end
-        rule_groups[flag].append({'word_suf': word_suf, "lemma_suf": suf})
-        # print('\t'.join([line, word_suf, suf]))
-    else:
-        print("UUUPS PROBLEM", i, l)
-with open("rules.json", 'w') as f:
-    json.dump(rule_groups, f)
+            raise Exception("Unexpected empty line! ({})".format(l))
 
-# with open("opisy_flag.txt", "w") as f:
-#     for flag, c  in comments.items():
-#         f.write("\n===== " + flag + " =====\n")
-#         f.write('\n'.join(c) + '\n')
+    with open(args.output, 'w') as f:
+        json.dump(rule_groups, f)
+
+    if args.verbose:
+        for f, r in rule_groups.items():
+            print(f, len(r))
+
+    if args.flags:
+        with open(args.description_file, "w") as f:
+            for flag, c in comments.items():
+                f.write("\n===== " + flag + " =====\n")
+                f.write('\n'.join(c) + '\n')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Extract rules from ispell aff file to more convenient form")
+    parser.add_argument('-r', '--ispell_rules', type=str, help="Ispell aff file path",
+                        default=settings.ISPELL_RULES)
+    parser.add_argument('-o', '--output', type=str, help="Output file path",
+                        default=settings.LEMMA_RULES)
+    parser.add_argument('-v', '--verbose', help="Display additional info",
+                        action='store_true')
+    parser.add_argument('-f', '--flags', help="Generate flags description file",
+                        action='store_true')
+    parser.add_argument('--description_file', type=str, help="Descrition file path",
+                        default=settings.FLAGS_DESC)
+    args = parser.parse_args()
+    main(args)
