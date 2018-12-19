34176689e78760f83caa6b490c4f8b7d6ce2751e
Piotr Książek <piotr.ksiazek96@gmail.com>
2018-12-19 01:53:33 +0100

    Change the paths to be relative from project's root directory Check if exists & create destination folder if needed

--- analysis/convert_nkjp.py
+++ analysis/convert_nkjp.py
@@ -2,13 +2,15 @@ import os
 import re
 import json
 
-with open('nkjp2us.json', 'r') as f:
+with open('./data/nkjp2us.json', 'r') as f:
     tag_conversion_map = json.load(f)
 
 tag_conversion = [{'old': k.lower(), 'new': v.lower()} for k, v in tag_conversion_map.items()]
 tag_conversion = sorted(tag_conversion, key=lambda x: len(x['new']))
-nkjp_path = os.path.expanduser('../data/NKJP_1.2_nltk/')
-nkjp_path_converted = os.path.expanduser('../data/NKJP_1.2_nltk/converted/')
+nkjp_path = os.path.expanduser('./data/NKJP_1.2_nltk/')
+nkjp_path_converted = os.path.expanduser(nkjp_path[:-1]+'_converted/')
+if not os.path.isdir(nkjp_path_converted):
+    os.mkdir(nkjp_path_converted)
 nkjp_files = os.listdir(nkjp_path)
 nkjp_files = [file for file in nkjp_files if os.path.isfile(os.path.join(nkjp_path, file))]
 fails = []
