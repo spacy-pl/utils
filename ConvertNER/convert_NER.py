a404e2dbe6d268f18fcac16e8130312e7705244d
Piotr Książek <piotr.ksiazek96@gmail.com>
2019-03-30 14:32:13 +0100

    Requested changes

--- ConvertNER/convert_NER.py
+++ ConvertNER/convert_NER.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+from spacy.lang.pl import Polish
 from spacy.gold import biluo_tags_from_offsets
 import spacy
 import json
@@ -120,7 +121,7 @@ def required_files_exist(dir):
 
     return True
 
-nlp = spacy.load('en_core_web_sm')
+nlp = Polish()
 doc_id = 0
 corpus = []
 
@@ -138,8 +139,8 @@ for f in os.listdir(os.path.join(path_prefix, corpus_path)):
     if not os.path.isdir((os.path.join(path_prefix,corpus_path,current_folder))):
         continue
 
+    # we skip the docs that don't have the required annotations (certain .xml files)
     if not required_files_exist(current_folder):
-        # doc_id +=1 ?
         continue
 
     tree_morphosyntax = ET.parse(os.path.join(path_prefix,corpus_path,current_folder,morphosyntax_xml))
