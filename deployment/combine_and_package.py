212e440ee48fdedd71bfd07664f4cb91ef2d62fd
Mateusz Olko <mateusz.olko@gmail.com>
2019-04-17 20:05:32 +0200

    Add option to supply some of pipeline elements (not all) to combine_and_package script

--- deployment/combine_and_package.py
+++ deployment/combine_and_package.py
@@ -1,3 +1,4 @@
+import tempfile
 import click
 import shutil
 
@@ -18,23 +19,26 @@ def combine(
         ner_path,
         blank_vectors_path,
 ):
-    pos = spacy.load(pos_path)
-    tree = spacy.load(tree_path)
-    ner = spacy.load(ner_path)
-
     combined = spacy.load(blank_vectors_path)
-    combined.add_pipe(get_component(pos, 'tagger', "POS Tagger"), 'tagger')
-    combined.add_pipe(get_component(tree, 'parser', "Dependency Tree Parser"), 'parser')
-    combined.add_pipe(get_component(ner, 'ner', "Entity Recognizer"), 'ner')
+
+    if pos_path:
+        pos = spacy.load(pos_path)
+        combined.add_pipe(get_component(pos, 'tagger', "POS Tagger"), 'tagger')
+    if tree_path:
+        tree = spacy.load(tree_path)
+        combined.add_pipe(get_component(tree, 'parser', "Dependency Tree Parser"), 'parser')
+    if ner_path:
+        ner = spacy.load(ner_path)
+        combined.add_pipe(get_component(ner, 'ner', "Entity Recognizer"), 'ner')
     return combined
 
 
 @click.command(help="Combine three trained models into one and package it for convenient use.")
-@click.option('--output-path', type=str)
-@click.option('--pos-path', type=str)
-@click.option('--tree-path', type=str)
-@click.option('--ner-path', type=str)
-@click.option('--blank-vectors-path', type=str)
+@click.option('-o', '--output-path', type=str)
+@click.option('-p', '--pos-path', type=str)
+@click.option('-t', '--tree-path', type=str)
+@click.option('-n', '--ner-path', type=str)
+@click.option('-b', '--blank-vectors-path', type=str)
 def main(
         pos_path,
         tree_path,
@@ -42,12 +46,10 @@ def main(
         output_path,
         blank_vectors_path,
 ):
-    TMP_MODEL_PATH = './models/tmp_for_package/'
     new_model = combine(pos_path, tree_path, ner_path, blank_vectors_path)
-    new_model.to_disk(TMP_MODEL_PATH)
-
-    cli.package(TMP_MODEL_PATH, output_path, create_meta=True)
-    shutil.rmtree(TMP_MODEL_PATH)
+    with tempfile.TemporaryDirectory() as tmp_model_path:
+        new_model.to_disk(tmp_model_path)
+        cli.package(tmp_model_path, output_path, create_meta=True)
 
 
 if __name__ == '__main__':
