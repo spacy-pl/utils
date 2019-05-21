be44c727e1f6456750f01adf799a1fb2d1c746d8
Ksiazek <piotr.ksiazek96@gmail.com>
2019-05-21 13:36:57 +0200

    Fix conversion script (messy atm)

--- ConvertNER/convert_NER_wroc.py
+++ ConvertNER/convert_NER_wroc.py
@@ -50,8 +50,8 @@ def process_token(tok):
     attribs = []
     orth = tok.find("orth").text
     for ann in tok.iter("ann"):
-        if ann.attrib['chan'].endswith("nam") and ann.text == "1":
-            attribs += [ann.attrib['chan']]
+        if ann.attrib['chan'].endswith("nam"): # and ann.text != "0":
+            attribs += [{ann.attrib['chan']:ann.text}]
 
     return Token(orth, attribs, -1)
 
@@ -84,6 +84,91 @@ def get_all_labels_with_cardinalities(tokens):
 def map_labels(tokens, map):
     for tok in tokens:
         tok.attribs = [map[attrib] for attrib in tok.attribs]
+
+    return tokens
+
+
+def get_longest_sequences(tokens):
+    res = []
+    b = 0
+    e = 0
+    # type = None
+
+    last_set = set()
+    while e != len(tokens)-1:
+        current_token = tokens[e]
+
+        if last_set == emptyset:
+            last_set = set(current_token.attribs)
+            b = e
+        else:
+            new_set = set(current_token.attribs) & last_set
+            if new_set == emptyset:
+                label = list(last_set)[0]
+                res.append((b, e, label))
+                b = e
+
+            last_set = new_set
+        e += 1
+
+    return res
+
+
+def still_in_sequence(v1, v2):
+    return any(v1e == v2e != "0" for v1e in v1 for v2e in v2)
+
+
+def get_last_label(v):
+    for i, e in enumerate(v):
+        if e != "0":
+            return i
+    return None
+
+
+def get_longest_sequences_2(tokens):
+    res = []
+    b = 0
+    e = 0
+    attribs = [k for d in tokens[0].attribs for k in d]
+    last_set = None
+
+    while e != len(tokens)-1:
+        current_token = tokens[e]
+
+        if last_set == None:
+            last_set = [v for d in current_token.attribs for k, v in d.items()]
+            b = e
+        else:
+            new_set = [v for d in current_token.attribs for k, v in d.items()]
+            if not still_in_sequence(last_set, new_set):
+                label_id = get_last_label(last_set)
+                if(label_id != None):
+                    label = attribs[label_id]
+                    res.append((b, e, label))
+                b = e
+
+            last_set = new_set
+        e += 1
+
+    return res
+
+
+emptyset = set()
+def pick_tags_2(tokens):
+    longest_sequences = get_longest_sequences_2(tokens)
+    res = []
+    for b, e, label in longest_sequences:
+        seq = tokens[b:e]
+        for tok in seq:
+            tok.attribs = [{label: '1'}]
+        # res += seq
+        tokens[b:e] = seq
+    return tokens
+
+
+def flatten_token_attrib_dicts(tokens):
+    for tok in tokens:
+        tok.attribs = [k for k in tok.attribs[0].keys()] if len(tok.attribs) > 0 is not None else []
 
     return tokens
 
@@ -205,8 +290,10 @@ def main(
                         token_idx += 1
                         tokens += [token]
 
-                    all_labels.merge(get_all_labels_with_cardinalities(tokens))  # for debug and analysis
-                    tokens = pick_tags(tokens)
+                    # all_labels.merge(get_all_labels_with_cardinalities(tokens))  # for debug and analysis
+                    tokens = pick_tags_2(tokens)
+                    tokens = flatten_token_attrib_dicts(tokens)
+
                     if use_label_map:
                         tokens = map_labels(tokens, NER_pwr_to_spacy)
                     tokens = convert_to_biluo(tokens)
